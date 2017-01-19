cc85a871a558fa22a0d2cf30429bc5f4b38b3dc6
James Callow <callow.james@gmail.com>
2017-01-19 10:59:30 -0500

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'mirai@example.com',
-    text: 'Hi. How are you doing.'
-  });
 });
 
 socket.on('disconnect', function () {
